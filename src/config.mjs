22ffe0d202a7de803fdcbdc4572cb02260722e4a
Miro Koczka <miroslavkoczka@gmail.com>
2022-08-20 11:45:09 +0200

    feat: Meta tags setup

--- src/config.mjs
+++ src/config.mjs
@@ -1,5 +1,6 @@
 export const SITE = {
   name: "Fitnes Senec",
+  description: "Posiľňovňa v Senci, ktorá tu je pre vás už viac ako 20 rokov.",
   domain: "https://fitnessenec.sk",
   github: "https://github.com/mkoczka/fitnes-senec",
 };
